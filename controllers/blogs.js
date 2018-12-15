44a37065dad3d2e2cde43ea74c72c8b373ddf5ee
Kaisa Saren <kaisa.saren@gmail.com>
2018-12-15 07:42:36 +0200

    4.19 asti, kirjautumistavan muutos

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -48,13 +57,35 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
+
+  const {
+    title,
+    author,
+    url,
+    likes,
+    userId,
+    content,
+  } = request.body
+
+
   try {
-    const { title, author, url, likes, userId } = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    if (content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+
     if (!title || !url ) {
       return response.status(400).json({ error: 'title or url missing' }).end()
     }
-
-    const user = await User.findById(userId)
 
     const blog1 = new Blog({
       title,
@@ -70,7 +101,12 @@ blogsRouter.post('/', async (request, response) => {
 
     response.json(Blog.format(result)).end()
   } catch(exception) {
-    response.status(400).end()
+    if (exception.name === 'JsonWebTokenError' ) {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'something went wrong...' })
+    }
   }
 })
 
